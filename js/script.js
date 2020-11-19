f4d3cb880021c9619cc21e99f98c3b008702bb9e
minteliu <minteliu.l@gmail.com>
2020-11-19 14:32:57 +0800

    Site updated: 2020-11-19 14:32:57

---
+++ js/script.js
@@ -0,0 +1,292 @@
+console.log("%c Github %c", "background:#333333; color:#ffffff", "", "https://github.com/izhaoo/hexo-theme-zhaoo");
+
+(function ($) {
+  "use strict";
+
+  var fn = {
+    showMenu: function () {
+      $(".menu").fadeIn(300);
+      $("body").addClass("lock-screen");
+      fn.hideFab();
+    },
+    hideMenu: function () {
+      $(".menu").fadeOut(300);
+      $("body").removeClass("lock-screen");
+    },
+    activeFab: function () {
+      $(".fab-up").addClass("fab-up-active");
+      $(".fab-plus").addClass("fab-plus-active");
+      $(".fab-daovoice").addClass("fab-daovoice-active");
+      $(".fab-tencent-chao").addClass("fab-tencent-chao-active");
+    },
+    freezeFab: function () {
+      $(".fab-up").removeClass("fab-up-active");
+      $(".fab-plus").removeClass("fab-plus-active");
+      $(".fab-daovoice").removeClass("fab-daovoice-active");
+      $(".fab-tencent-chao").removeClass("fab-tencent-chao-active");
+    },
+    showFab: function () {
+      $(".fab").removeClass("fab-hide").addClass("fab-show");
+    },
+    hideFab: function () {
+      $(".fab").addClass("fab-hide").removeClass("fab-show");
+    },
+    scroolFab: function () {
+      var height = $(window).height();
+      var scrollTop = $(window).scrollTop();
+      if (scrollTop > height) {
+        fn.showFab();
+      } else {
+        fn.freezeFab();
+        fn.hideFab();
+      }
+    },
+    scroolToTop: function () {
+      $('body,html').animate({
+        scrollTop: '0px'
+      }, 800);
+    },
+    navbar: {
+      mobile: function () {
+        $(".navbar").addClass("hide");
+        $(window).on("scroll", ZHAOO.utils.throttle(function () {
+          $(window).on("scroll", function () {
+            var scrollTop = $(this).scrollTop();
+            if (scrollTop > 250) {
+              $(".navbar").removeClass("hide");
+            } else {
+              $(".navbar").addClass("hide");
+            }
+          })
+        }, 500));
+      },
+      desktop: function () {
+        function center() {
+          if ($(window).scrollTop() > 60) {
+            $(".navbar .center").addClass("hide");
+          } else {
+            $(".navbar .center").removeClass("hide");
+          }
+        }
+        center();
+        $(window).on("scroll", ZHAOO.utils.throttle(function () {
+          center();
+        }, 500));
+      },
+    },
+    motto: function () {
+      if (CONFIG.preview.motto.api) {
+        $.get(CONFIG.preview.motto.api, function (data) {
+          if (data) {
+            $("#motto").text(data);
+          }
+        });
+      }
+    },
+    background: function () {
+      if (CONFIG.preview.background.api) {
+        $(".preview-image").css("background-image", "url(" + CONFIG.preview.background.api + ")");
+      }
+    }
+  }
+
+  var action = {
+    smoothScroll: function () {
+      // a[href *=#], area[href *=#]
+      $(".smooth-scroll, .toc-link").click(function () {
+        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+          var $target = $(decodeURIComponent(this.hash));
+          $target = $target.length && $target || $("[name=" + this.hash.slice(1) + "]");
+          if ($target.length) {
+            var targetOffset = $target.offset().top;
+            $("html,body").animate({
+              scrollTop: targetOffset
+            }, 500);
+            location.hash = this.hash;
+            return false;
+          }
+        }
+      });
+    },
+    loading: function () {
+      $(".loading").delay(500).fadeOut(300);
+      $("body").removeClass("lock-screen");
+    },
+    fab: function () {
+      $(".fab-plus").on("click", function () {
+        if ($(this).hasClass("fab-plus-active")) {
+          fn.freezeFab();
+        } else {
+          fn.activeFab();
+        }
+      });
+      $(".fab-daovoice").on("click", function () {
+        daovoice('openMessages');
+      });
+      $(".fab-up, .fab-daovoice, fab-tencent-chao").on("click", function () {
+        fn.freezeFab();
+      });
+      if (CONFIG.fab.always_show) {
+        fn.showFab();
+      } else {
+        $(window).scroll(fn.scroolFab);
+      }
+    },
+    menu: function () {
+      $(".menu-close").on("click", function () {
+        fn.hideMenu();
+        $(".navbar").removeClass("hide");
+      });
+    },
+    scroolToTop: function () {
+      $(".fab-up").on("click", function () {
+        fn.scroolToTop();
+      })
+    },
+    fancybox: function () {
+      $(".fancybox").fancybox();
+      $(".article .content img").each(function () {
+        var e = document.createElement("a");
+        $(e).attr("data-fancybox", "images");
+        $(e).attr("href", $(this).attr("src"));
+        $(this).wrap(e);
+      });
+    },
+    pjax: function () {
+      $(function () {
+        $(document).pjax("a:not(.menu *)", '#main', {
+          fragment: '#main',
+          timeout: 6000
+        });
+      });
+      $(document).on('pjax:complete', function () {
+        if (CONFIG.fancybox) {
+          action.fancybox();
+        }
+      });
+    },
+    donate: function () {
+      $(".donate .icon").on("mouseover", function () {
+        $("#qrcode-donate").show();
+      });
+      $(".donate .icon").children("a").on("mouseover", function () {
+        $("#qrcode-donate img").attr('src', eval('CONFIG.donate.' + $(this).attr('id')))
+      });
+      $(".donate .icon").on("mouseout", function () {
+        $("#qrcode-donate").hide();
+      });
+    },
+    lazyload: function () {
+      $("img.lazyload").lazyload({
+        effect: "fadeIn",
+        threshold: 200,
+      });
+    },
+    fixLazyloadFancybox: function () {
+      $(document).find('.article img[data-original]').each(function () {
+        $(this).parent().attr("href", $(this).attr("data-original"));
+      });
+    },
+    carrier: function () {
+      $(".j-carrier-btn").on("click", ZHAOO.utils.throttle(function () {
+        $(".j-carrier-data").select();
+        document.execCommand("Copy");
+        ZHAOO.zui.message({ text: '已复制到剪切板', type: 'success' });
+      }, 1000));
+    },
+    navbar: function () {
+      $(window).resize(ZHAOO.utils.throttle(function () {
+        if (ZHAOO.utils.isDesktop()) {
+          fn.navbar.desktop();
+        }
+        if (ZHAOO.utils.isMobile() && !CONFIG.isHome) {
+          fn.navbar.mobile();
+        }
+      }, 1000)).resize();
+      $(".j-navbar-menu").on("click", function () {
+        fn.showMenu();
+        $(".navbar").addClass("hide");
+        $(".qrcode").fadeOut(300);
+      });
+      $(".j-navbar-qrcode").on("click", function () {
+        if ($("#qrcode-navbar").is(":hidden")) {
+          $("#qrcode-navbar").fadeIn(300);
+        } else {
+          $("#qrcode-navbar").fadeOut(300);
+        }
+      });
+    },
+    preview: function () {
+      fn.background();
+      fn.motto();
+    },
+    qrcode: function () {
+      if (CONFIG.qrcode.type === 'url') {
+        $("#qrcode-navbar").qrcode({
+          text: window.location.href,
+          width: 150,
+          height: 150
+        });
+      } else if (CONFIG.qrcode.type === 'image') {
+        $("#qrcode-navbar").append('<img src="' + CONFIG.qrcode.image + '" alt="qrcode" />');
+      }
+    },
+    toc: function () {
+      var current = [];
+      var titleList = new Map();
+      $("article .content h1,h2,h3,h4,h5,h6").each(function () {
+        var title = $(this).attr("id");
+        var height = $(this).offset().top;
+        titleList.set(height, title);
+      });
+      $(window).on("scroll", f);
+      function f() {
+        var height = $(this).scrollTop() || $(window).scrollTop();
+        for (var item of titleList) {
+          if (item[0] >= height) {
+            current = item;
+            break;
+          }
+        }
+        $(".toc-link").removeClass("active");
+        $(".toc-link[href='#" + current[1] + "']").addClass("active");
+      };
+      f();
+    }
+  }
+
+  $(function () {
+    action.smoothScroll();
+    action.loading();
+    action.fab();
+    action.navbar();
+    action.menu();
+    action.scroolToTop();
+    action.preview();
+    if (CONFIG.fancybox) {
+      action.fancybox();
+    }
+    if (CONFIG.pjax) {
+      action.pjax();
+    }
+    if (CONFIG.lazyload.enable) {
+      action.lazyload();
+    }
+    if (CONFIG.donate.enable) {
+      action.donate();
+    }
+    if (CONFIG.lazyload && CONFIG.fancybox) {
+      action.fixLazyloadFancybox();
+    }
+    if (CONFIG.carrier.enable) {
+      action.carrier();
+    }
+    if (CONFIG.qrcode.enable) {
+      action.qrcode();
+    }
+    if (CONFIG.toc.enable) {
+      action.toc();
+    }
+  });
+
+})(jQuery);
---
+++ js/utils.js
@@ -0,0 +1,125 @@
+(function ($) {
+  "use strict";
+
+  ZHAOO.utils = {
+    debounce: function (func, wait, immediate) {
+      var timeout;
+      return function () {
+        var context = this;
+        var args = arguments;
+        if (timeout) clearTimeout(timeout);
+        if (immediate) {
+          var callNow = !timeout;
+          timeout = setTimeout(function () {
+            timeout = null;
+          }, wait);
+          if (callNow) func.apply(context, args);
+        } else {
+          timeout = setTimeout(function () {
+            func.apply(context, args);
+          }, wait);
+        }
+      }
+    },
+    throttle: function (func, wait, options) {
+      var timeout, context, args;
+      var previous = 0;
+      if (!options) options = {};
+      var later = function () {
+        previous = options.leading === false ? 0 : new Date().getTime();
+        timeout = null;
+        func.apply(context, args);
+        if (!timeout) context = args = null;
+      }
+      var throttled = function () {
+        var now = new Date().getTime();
+        if (!previous && options.leading === false) previous = now;
+        var remaining = wait - (now - previous);
+        context = this;
+        args = arguments;
+        if (remaining <= 0 || remaining > wait) {
+          if (timeout) {
+            clearTimeout(timeout);
+            timeout = null;
+          }
+          previous = now;
+          func.apply(context, args);
+          if (!timeout) context = args = null;
+        } else if (!timeout && options.trailing !== false) {
+          timeout = setTimeout(later, remaining);
+        }
+      }
+      return throttled;
+    },
+    hasMobileUA: function () {
+      var nav = window.navigator;
+      var ua = nav.userAgent;
+      var pa = /iPad|iPhone|Android|Opera Mini|BlackBerry|webOS|UCWEB|Blazer|PSP|IEMobile|Symbian/g;
+      return pa.test(ua);
+    },
+    isTablet: function () {
+      return (
+        window.screen.width > 767 &&
+        window.screen.width < 992 &&
+        this.hasMobileUA()
+      );
+    },
+    isMobile: function () {
+      return window.screen.width < 767 && this.hasMobileUA();
+    },
+    isDesktop: function () {
+      return !this.isTablet() && !this.isMobile();
+    }
+  }
+
+  ZHAOO.zui = {
+    message: function ({ text, type, timer }) {
+      var message = '<div class="zui-message ' + (type || "info") + '"><p>' + text + '</p></div>';
+      $("body").append(message);
+      var e = $(".zui-message");
+      e.ready(function () {
+        e.addClass("in");
+        setTimeout(function () {
+          e.removeClass("in");
+          e.on("transitionend webkitTransitionEnd", function () {
+            $(this).remove();
+          });
+        }, timer || 3000);
+      });
+    }
+  }
+
+})(jQuery);
+
+class AsyncLimit {
+  constructor(limit) {
+    this.limit = Number(limit) || 2;
+    this.pool = [];
+    this.current = 0;
+  }
+
+  async run(fn) {
+    if (!fn || typeof fn !== 'function') {
+      throw new Error('Function error.');
+    }
+    if (this.current >= this.limit) {
+      await new Promise(resolve => this.pool.push(resolve));
+    }
+    return this._handleRun(fn);
+  }
+
+  async _handleRun(fn) {
+    this.current++;
+    try {
+      return await fn();
+    } catch (err) {
+      return Promise.reject(err);
+    } finally {
+      this.current--;
+      if (this.pool.length) {
+        this.pool[0]();
+        this.pool.shift();
+      }
+    }
+  }
+}
